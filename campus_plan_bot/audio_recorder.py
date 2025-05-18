91db65559531b358a6f86f830b4cbc0fa8bd9011
LunaNordin <56644187+LunaNordin@users.noreply.github.com>
2025-05-18 14:57:26 +0200

    Forgot pre-commits
    Forgot to re-setup the pre-commit hooks after tinkering with pixi. Updated last changes to pass the checks

--- campus_plan_bot/audio_recorder.py
+++ campus_plan_bot/audio_recorder.py
@@ -1,10 +1,12 @@
-import pyaudio
-import wave
 import threading
+import wave
+
+import pyaudio
 from pynput import keyboard
 
+
 class AudioRecorder:
-    def __init__(self, filename='out.wav', channels=1, rate=44100, chunk=1024):
+    def __init__(self, filename="out.wav", channels=1, rate=44100, chunk=1024):
         self.filename = filename
         self.channels = channels
         self.rate = rate
@@ -19,14 +21,15 @@ class AudioRecorder:
         """Start recording audio in a background thread."""
         self.frames = []
 
-        
         self.recording = True
-        stream = self.p.open(format=pyaudio.paInt16,
-                                   channels=self.channels,
-                                   rate=self.rate,
-                                   input=True,
-                                   frames_per_buffer=self.chunk)
-        print("Recording started...", end='')
+        stream = self.p.open(
+            format=pyaudio.paInt16,
+            channels=self.channels,
+            rate=self.rate,
+            input=True,
+            frames_per_buffer=self.chunk,
+        )
+        print("Recording started...", end="")
 
         while self.recording:
             data = stream.read(self.chunk)
@@ -42,17 +45,17 @@ class AudioRecorder:
         self.recording = False
 
         # Save the recorded data as a .wav file
-        with wave.open(self.filename, 'wb') as wf:
+        with wave.open(self.filename, "wb") as wf:
             wf.setnchannels(self.channels)
             wf.setsampwidth(self.p.get_sample_size(pyaudio.paInt16))
             wf.setframerate(self.rate)
-            wf.writeframes(b''.join(self.frames))
+            wf.writeframes(b"".join(self.frames))
         self.frames.clear()
 
     def start(self):
         self.recording_thread = threading.Thread(target=self.record)
         self.recording_thread.start()
-    
+
     def stop(self):
         self.end_recording()
         self.recording_thread.join()
@@ -67,7 +70,9 @@ class AudioRecorder:
             return False
 
     def record_audio(self):
-        self.listener = keyboard.Listener(on_press=self.on_press, on_release=self.on_release, suppress=True)
+        self.listener = keyboard.Listener(
+            on_press=self.on_press, on_release=self.on_release, suppress=True
+        )
         self.listener.start()
         print("Press and hold the spacebar to record your input.")
 
