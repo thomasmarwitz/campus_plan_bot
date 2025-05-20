2bbc73e2c891039e4941fb28ffd01e8756a51b57
thomasmarwitz <thomasmarwitz3@gmail.com>
2025-05-20 12:08:42 +0200

    POC: simple rag system and more advanced system prompt.
    RAG is done every time (this should be changed, use a multi step pipeline where the model is instructed to decide when to do RAG).
    RAG is a two step process: checking for building number matches using REGEX (numbers are not well embedded) and embedding using
    sentence transformers.

--- campus_plan_bot/cli.py
+++ campus_plan_bot/cli.py
@@ -1,22 +1,28 @@
+from pathlib import Path
+
 import click
 
 from campus_plan_bot.bot import SimpleTextBot
+
+database_path = Path("phase1") / "data" / "campusplan_evaluation.csv"
 
 
 @click.command()
 def chat():
     """Simple CLI chatbot interface."""
-    click.echo("Welcome to the Chatbot CLI. Type 'exit' to quit.")
-    bot = SimpleTextBot()
+    click.echo(
+        "Welcome to the chat with CampusGuide, you can ask questions about buildings, opening hours, navigation. Type 'exit' to quit."
+    )
+    bot = SimpleTextBot(database_path)
 
     while True:
-        user_input = click.prompt("You")
+        user_input: str = click.prompt("You")
         if user_input.strip().lower() in {"exit", "quit"}:
             click.echo("Goodbye!")
             break
 
         response = bot.query(user_input)
-        click.echo(f"Bot: {response}")
+        click.echo(f"{bot.name}: {response}")
 
 
 if __name__ == "__main__":
