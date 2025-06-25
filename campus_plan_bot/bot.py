5830734a845a98121cdb4a05685ca6760718837a
LunaNordin <56644187+LunaNordin@users.noreply.github.com>
2025-06-25 12:13:10 +0200

    Separated bot and RAG
    Moved the document retrieval out of the bot's query method to allow for independent adjustment of RAG, document post-processing and model querying

--- campus_plan_bot/bot.py
+++ campus_plan_bot/bot.py
@@ -1,19 +1,8 @@
-import os
-import re
-from collections.abc import Sequence
 from datetime import datetime
-from pathlib import Path
-
-import numpy as np
-import pandas as pd
-from loguru import logger
-from sentence_transformers import SentenceTransformer
-from sentence_transformers.util import cos_sim
 
 from campus_plan_bot.interfaces.interfaces import (
     LLMRequestConfig,
     MessageProtocol,
-    RAGComponent,
     RetrievedDocument,
     TextBot,
 )
@@ -94,90 +83,10 @@ class LLama3PromptBuilder:
         return f"<|begin_of_text|>{system_message}{''.join(messages)}<|start_header_id|>assistant<|end_header_id|>"
 
 
-class RAG(RAGComponent):
-
-    MODEL = "all-MiniLM-L6-v2"
-
-    def __init__(self, embedding_data: list, database: pd.DataFrame):
-        os.environ["TOKENIZERS_PARALLELISM"] = "true"
-
-        self.embedding_data = embedding_data
-        self.database = database
-        logger.debug(f"Loading {self.MODEL} model...")
-        self.model = SentenceTransformer(self.MODEL)
-        logger.debug(f"Embedding {len(embedding_data)} documents...")
-        self.embeddings = self.model.encode(embedding_data, convert_to_tensor=True)
-        logger.debug("Embeddings loaded.")
-
-    @classmethod
-    def from_file(cls, file_path: Path) -> "RAG":
-        """Create a RAG instance from a file."""
-        df = pd.read_csv(file_path)
-        return cls.from_df(df)
-
-    @classmethod
-    def from_df(cls, df: pd.DataFrame) -> "RAG":
-        """Create a RAG instance from a DataFrame."""
-        embedding_data = df["identifikator"].tolist()
-        return cls(embedding_data, df)
-
-    def retrieve_context(self, query: str, limit: int = 5) -> list[RetrievedDocument]:
-        """Retrieve relevant context based on a query string."""
-        # 2 step approach
-        documents: list[RetrievedDocument] = []
-
-        # 1. check whether building number of type 50.34 (1-2 numbers).(1-2 numbers) do exactly match
-        PATTERN = r"(\d{1,2}\.\d{1,2})"
-        mo = re.search(PATTERN, query)
-        if mo:
-            building_number = mo.group(0)
-            logger.debug(f"Building number found: {building_number}")
-            related_documents = [
-                RetrievedDocument(
-                    id=title,
-                    content=str(self.database.iloc[index].to_dict()),
-                    relevance_score=1.0,
-                )
-                for index, title in enumerate(self.embedding_data)
-                if building_number in title
-            ][:limit]
-            documents.extend(related_documents)
-            logger.debug(
-                f"Found {len(related_documents)} documents matching the building number."
-            )
-
-        # 2. if not, use cosine similarity to find the most relevant documents
-        if len(documents) < limit:
-            top_k = limit - len(documents)
-
-            query_embedding = self.model.encode(query, convert_to_tensor=True)
-            cos_scores = (
-                cos_sim(query_embedding, self.embeddings)[0].cpu().numpy()
-            )  # does this work if data already on cpu?
-            top_k_indices: Sequence[int] = np.argsort(-cos_scores)[:top_k]  # type: ignore[assignment]
-
-            for score, index in zip(cos_scores[top_k_indices], top_k_indices):
-                documents.append(
-                    RetrievedDocument(
-                        id=self.database.loc[index]["identifikator"],
-                        content=str(self.database.iloc[index].to_dict()),
-                        relevance_score=round(float(score), 3),
-                    )
-                )
-            logger.debug(f"Found {top_k} documents using cosine similarity.")
-
-        logger.debug(
-            "Retrieved "
-            + ", ".join(f"({doc.relevance_score}) {doc.id}" for doc in documents)
-        )
-        return documents
-
-
 class SimpleTextBot(TextBot):
 
     def __init__(
         self,
-        rag: RAG,
         prompt_builder: LLama3PromptBuilder | None = None,
         llm_client: LLMClient | None = None,
     ):
@@ -190,25 +99,12 @@ class SimpleTextBot(TextBot):
             )
         )
         self.prompt_builder = prompt_builder or LLama3PromptBuilder()
-        self.rag = rag
 
-    @classmethod
-    def from_file(
-        cls,
-        database_p: Path,
-        prompt_builder: LLama3PromptBuilder | None = None,
-        llm_client: LLMClient | None = None,
-    ) -> "SimpleTextBot":
-        # moved from init to separate method to allow to compute RAG only once and not for every bot instance
-        return cls(RAG.from_file(database_p), prompt_builder, llm_client)
-
-    def query(self, query: str) -> str:
+    def query(self, query: str, docs: list[RetrievedDocument]) -> str:
         user_query = Message.from_content(query, Role.USER)
 
-        documents = self.rag.retrieve_context(query, limit=5)
-
         rag_message = Message.from_content(
-            "\n".join([str(doc) for doc in documents]), Role.USER
+            "\n".join([str(doc) for doc in docs]), Role.USER
         )
 
         self.conversation_history.add_message(user_query)
